f6230fc5cec3ba45ea11ecf21b0d978c523e44fe
lgtinoco <gabrieladiaztinoco@gmail.com>
2021-09-13 14:31:46 -0600

    componente de diputados funcional

--- src/pages/partidos.js
+++ src/pages/partidos.js
@@ -14,12 +14,12 @@ import 'react-multi-carousel/lib/styles.css';
 const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 5,
       slidesToSlide: 3 // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 3,
       slidesToSlide: 2 // optional, default to 1.
     },
     mobile: {
@@ -56,40 +56,40 @@ export default function Partidos() {
                 <Historia/>
                 <Proyectos/>
                 <SerParte/>
-                
-                <Carousel
-                    swipeable={false}
-                    draggable={false}
-                    showDots={true}
-                    responsive={responsive}
-                    ssr={true} // means to render carousel on server-side.
-                    infinite={true}
-                    autoPlay={true}
-                    autoPlaySpeed={1000}
-                    keyBoardControl={true}
-                    customTransition="all .5"
-                    transitionDuration={500}
-                    containerClass="carousel-container"
-                    removeArrowOnDeviceType={["tablet", "mobile"]}
-                    dotListClass="custom-dot-list-style"
-                    itemClass="carousel-item-padding-40-px"
-                >
-                    {data.vacationPhotos.edges.map(image => (
-                        <div class="max-w-sm sm:w-1/2 md:w-1/2 lg:w-1/3">
-                           <div class="team-item">
-                                <div class="team-img relative">
-                                    <Img 
-                                        fluid={image.node.childImageSharp.fluid}
-                                        alt={image.node.base.split('-').join(' ').split('.')[0]}
-                                    />        
-                                </div>
-                                <div class="text-center px-5 py-3">
-                                    <h3 class="team-name">{image.node.base.charAt(0).toUpperCase() + image.node.base.substr(1).split('-').join(' ').split('.')[0]}</h3>
-                                </div>
-                            </div>    
-                        </div>
-                    ))} 
-                </Carousel>
+                <section id="team" class="py-24 text-center">
+                    <div class="text-center">
+                        <h2 class="mb-12 section-heading wow fadeInDown animated" data-wow-delay="0.3s">Diputados</h2>
+                    </div>
+                        <Carousel
+                            swipeable={false}
+                            draggable={false}
+                            showDots={false}
+                            responsive={responsive}
+                            ssr={true} // means to render carousel on server-side.
+                            infinite={true}
+                            autoPlay={true}
+                            autoPlaySpeed={1000}
+                            keyBoardControl={true}
+                            customTransition="all .5"
+                            transitionDuration={500}
+                            containerClass="carousel-container"
+                            removeArrowOnDeviceType={["tablet", "mobile"]}
+                            itemClass="carousel-item-padding-40-px"
+                        >
+                          {data.vacationPhotos.edges.map(image => (
+                            <div>
+                                <Img 
+                                    fluid={image.node.childImageSharp.fluid}
+                                    alt={image.node.base.split('-').join(' ').split('.')[0]}
+                                 />
+                          
+                                <p>{image.node.base.charAt(0).toUpperCase() + image.node.base.substr(1).split('-').join(' ').split('.')[0]}</p>
+                            </div>
+                                 
+                          ))} 
+                            
+                        </Carousel>   
+                </section>
             </Layout>
 
         )}
